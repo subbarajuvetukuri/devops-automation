604a64f792c312ea193e6decbb5a8a566f99f3ce
subbarajuvetukuri <subbaraju.vetukuri@gmail.com>
2023-02-10 15:52:38 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Hi Friends Latest Devops Projects Uploaded Daily Based . LiKe & Share this video & also Subscribe";
+		return "Hi, Welcome to hyderabad niu";
 	}
 
 	public static void main(String[] args) {
